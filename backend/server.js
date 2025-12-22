ddcd58487c08e250f05f1d1b96b3931b10cd551c
lucasmri <lucfreela0202@gmail.com>
2025-12-22 14:03:09 -0300

    Starting backend

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const porta = 3000;
+app.use(express.json());
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}`);
+});
