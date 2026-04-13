92dee0909995bb68d16a5ffc67c86de84dad3da0
lucasmri <lucfreela0202@gmail.com>
2026-04-12 23:55:20 -0300

    started TarefaController

---
+++ backend/src/main/java/com/messias/taskear/controller/TarefaController.java
@@ -0,0 +1,18 @@
+package com.messias.taskear.controller;
+
+import com.messias.taskear.service.TarefaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tarefa")
+public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+
+}
