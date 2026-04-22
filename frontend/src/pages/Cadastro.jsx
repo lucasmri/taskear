7c61e7b26c8dcca0154e073fd0775f00d13313ef
lucasmri <lucfreela0202@gmail.com>
2026-04-21 23:26:41 -0300

    Started front end

--- frontend/src/pages/Cadastro.jsx
+++ frontend/src/pages/Cadastro.jsx
@@ -1,28 +1,65 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Cadastro() {
+
+  const navigate = useNavigate();
+  const [form, setForm] = useState({ nome: "", email: "", senhaHash: "" });
+
+  function handleNomeChange(e) {
+    setForm({ ...form, nome: e.target.value });
+  }
+
+  function handleEmailChange(e) {
+    setForm({ ...form, email: e.target.value });
+  }
+
+  function handleSenhaChange(e) {
+    setForm({ ...form, senhaHash: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:8080/usuario", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(form),
+      });
+
+      if (res.status === 409) {
+        alert("E-mail já castrado!");
+        return;
+      }
+      navigate("/");
+    } catch {
+      alert("Erro na requisição!");
+    }
+  }
+  
   return (
     <div id="cadastro-page">
 
       <div className="cadastro-container">
 
-        <form id="cadastro-form">
+        <form id="cadastro-form" onSubmit={handleSubmit}>
 
           <h2>Cadastro</h2>
 
           <div className="form-group">
             <label htmlFor="cadastro-name">Nome</label>
-            <input type="text" placeholder="nome" id="cadastro-name" />
+            <input type="text" placeholder="nome" id="cadastro-name" name="nome" value={form.nome} onChange={handleNomeChange} required/>
           </div>
 
           <div className="form-group">
             <label htmlFor="cadastro-email">E-mail</label>
-            <input type="email" placeholder="@email.com" id="cadastro-email" />
+            <input type="email" placeholder="@email.com" id="cadastro-email" name="email" value={form.email} onChange={handleEmailChange} required/>
           </div>
 
           <div className="form-group">
             <label htmlFor="cadastro-password">Senha</label>
-            <input type="password" placeholder="senha" id="cadastro-password" />
+            <input type="password" placeholder="senha" id="cadastro-password" name="senhaHash" value={form.senhaHash} onChange={handleSenhaChange} required/>
           </div>
 
           <div className="form-actions">
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,60 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+  const navigate = useNavigate();
+  const [form, setForm] = useState({ email: "", senha: "" });
+
+  function handleEmailChange(e) {
+    setForm({ ...form, email: e.target.value });
+  }
+
+  function handleSenhaChange(e) {
+    setForm({ ...form, senha: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      });
+
+      if (res.status === 401) {
+        alert("E-mail ou senha inválidos!");
+        return;
+      }
+
+      const usuario = await res.json();
+      localStorage.setItem("usuario", JSON.stringify(usuario));
+
+      navigate("/Home");
+    } catch {
+      alert("Erro na requisição!");
+    }
+  }
+
   return (
     <div id="login-page">
 
       <div className="login-container">
 
-        <form id="login-form">
+        <form id="login-form" onSubmit={handleSubmit}>
 
           <h2>Login</h2>
 
           <div className="form-group">
             <label htmlFor="login-email">E-mail</label>
-            <input type="email" placeholder="@email.com" id="login-email"/>
+            <input type="email" placeholder="@email.com" id="login-email" name="email" value={form.email} onChange={handleEmailChange} required/>
           </div>
 
           <div className="form-group">
             <label htmlFor="login-password">Senha</label>
-            <input type="password" placeholder="senha" id="login-password"/>
+            <input type="password" placeholder="senha" id="login-password" name="senha" value={form.senha} onChange={handleSenhaChange} required/>
           </div>
 
           <div className="form-actions">
