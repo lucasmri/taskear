dcbc781d550342477d4a9485f372aa16e04c6ad5
lucasmri <lucfreela0202@gmail.com>
2026-05-16 20:18:06 -0300

    style(home): finish dashboard layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,47 +1,51 @@
 import Menu from '../components/Menu.jsx';
 import GroupsIcon from '@mui/icons-material/Groups';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import SyncAltIcon from '@mui/icons-material/SyncAlt';
+import TaskIcon from '@mui/icons-material/Task';
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div id="home-page">
+    <div className={styles.home_page}>
 
       <Menu />
 
-      <main className="teams-section">
+      <main className={styles.teams_section}>
 
-        <div className="cards-status">
+        <div className={styles.cards_status}>
           
-          <div className="card-pending">
-            <GroupsIcon style={{ color: "blue", fontSize: 32 }}/>
-            <div className="card-pending-info">
-              <p className="card-pending-info-label">Tarefas pendentes</p>
-              <p className="card-pending-info-value">10</p>
+          <div className={styles.card_peding}>
+            <PendingActionsIcon style={{ color: "#F2B718", fontSize: 80 }}/>
+            <div className={styles.card_info}>
+              <p className={styles.card_info_label}>Tarefas pendentes</p>
+              <p className={styles.card_info_value}>0</p>
             </div>
           </div>
 
-          <div className="card-doing">
-            <GroupsIcon style={{ color: "blue", fontSize: 32 }}/>
-            <div className="card-doing-info">
-              <p className="card-doing-info-label">Tarefas em andamentos</p>
-              <p className="card-doing-info-value">02</p>
+          <div className={styles.card_doing}>
+            <SyncAltIcon style={{ color: "#1F60B5", fontSize: 80 }}/>
+            <div className={styles.card_info}>
+              <p className={styles.card_info_label}>Tarefas em andamentos</p>
+              <p className={styles.card_info_value}>0</p>
             </div>
           </div>
 
-          <div className="card-done">
-            <GroupsIcon style={{ color: "blue", fontSize: 32 }}/>
-            <div className="card-done-info">
-              <p className="card-done-info-label">Tarefas concluídas</p>
-              <p className="card-done-info-value">29</p>
+          <div className={styles.card_done}>
+            <TaskIcon style={{ color: "#25703B", fontSize: 80 }}/>
+            <div className={styles.card_info}>
+              <p className={styles.card_info_label}>Tarefas concluídas</p>
+              <p className={styles.card_info_value}>0</p>
             </div>
           </div>
 
         </div>
 
-        <div className="teams-card">
-          <GroupsIcon style={{ color: "blue", fontSize: 32 }}/>
-          <div className="teams-card-info">
-            <p className="teams-card-label">Minhas equipes</p>
-            <p className="teams-card-value">02</p>
+        <div className={styles.teams_card}>
+          <GroupsIcon style={{ color: "#1F60B5", fontSize: 280 }}/>
+          <div className={styles.teams_card_info}>
+            <p className={styles.teams_card_label}>Minhas equipes</p>
+            <p className={styles.teams_card_value}>0</p>
           </div>
         </div>
 
